c779d36944aa7a87130c506722c4968b66ba1636
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-12 17:30:08 +0000

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api do
+    namespace :v1 do
+      get 'greeting' => 'greetings#random_greeting'
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'root#index'
 end
