750cb92c63827b6d0209d8b2fdd8e062695d4c4e
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-12 18:13:53 +0000

    create App.js component file

---
+++ app/javascript/components/App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getGreetings } from '../redux/greetings/greetingsSlice';
+
+import Greeting from './Greeting';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  );
+}
+
+export default App;
