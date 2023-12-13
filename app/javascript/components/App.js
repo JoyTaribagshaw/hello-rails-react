b1ea782bd45bac0f2fc575939db9797c22d0bc89
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-13 18:27:38 +0000

    update App.js with eact route

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greeting from "./Greeting";
+
 const App = ()=>{
 
-    return < Greeting />
+    return (
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Greeting/>}/>
+          </Routes>
+        </Router>
+      );
 }
 
 export default App;
